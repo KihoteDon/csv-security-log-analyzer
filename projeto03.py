6260141f1e7a21961eb7ec18fbfc6a585361eec9
DonQuixote <KihoteDon@proton.me>
2026-03-18 20:56:16 -0300

    Adicionando filtro e ranking de IP

--- projeto03.py
+++ projeto03.py
@@ -5,6 +5,38 @@
 
 
 from tqdm import tqdm
+import csv
+
+def extrair_ip():
+    # Função para extrair os IPs e adicionar a um dicionário com sua respectiva contagem
+    with open ("archive/logs.csv", newline="") as file:
+        lista_ip = {
+
+        }
+        reader = csv.reader(file)
+        next(reader)
+
+        for linha in tqdm(reader, total=6000001, desc="Analisando e Validando IPs..."):
+            saida = linha[1]
+            partes = saida.split(".")
+            valido = True
+            
+            if len(partes) != 4:
+                continue
+            for i in partes:
+                if not i.isdigit():
+                    valido = False
+                    break
+                elif int(i) > 255 or int(i) < 0:
+                    valido = False
+                    break 
+            if valido == True:
+                lista_ip[saida] = lista_ip.get(saida, 0) + 1
+
+        # Retornando dicionário de IPs Válidos
+        return lista_ip
+            
+            
 
 # Função para filtrar dados com base da entrada do usuário
 def filtrar_dado():
@@ -22,6 +54,7 @@ def filtrar_dado():
 
 # A estatística geral basicamente retorna quantidade de blocked/allowed, Ranking IP, Ranking Protocolos, Ferramentas utilizadas
 # Ranking fonte de logs, e estatística temporal
+
 def estatistica_geral():
     total_linha = 6000001 # Quantidade de linhas que o log possui
     count = 0
@@ -49,7 +82,26 @@ def estatistica_geral():
                     dir.write(linha)
             print(f"A quantidade de blocked dentro do dataset é de {count} e a porcentagem em relação ao mesmo é de {round(count/total_linha*100, 2)}%")
 
-    
+            file.seek(0)
+            count = 0
+
+def estatistica_ip():
+    with open ("rep.txt", "w") as repositorio:
+        dicionario = extrair_ip()
+        
+        rankear = sorted(dicionario.items(), key=lambda item: item[1], reverse=True)
+        print(rankear[:5])
+        
+
+
+        
+
+                
+                
+                
+
+
+
 
 # Função principal
 def main():
@@ -59,11 +111,13 @@ def main():
         entrada = int(input("Que tipo de inspeção deseja fazer: "))
     except ValueError:
         print("Entrada inválida")
-
+        
     if entrada == 1:
         filtrar_dado()
     if entrada == 2:
         estatistica_geral()
+    if entrada == 3:
+        estatistica_ip()
     else:
         print("wip")
 
